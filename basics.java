458019e73094601fa2d36e23428cf67b30d6e1ca
Dhinakaranv01 <dhinavasudevan@gmail.com>
2023-11-30 22:15:56 +0530

    casting programs

---
+++ basics.java
@@ -0,0 +1,14 @@
+public class basics{
+    public static void main(String[]args){
+        // widening type casting
+        int a = 10;
+        double b= a;
+        System.out.println(b);
+        
+        // narroe type casting
+        double c =12.0;
+        int  d =(int)c;
+        System.out.println(d);
+    }
+}
+        
