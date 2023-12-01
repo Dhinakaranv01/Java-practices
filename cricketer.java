2352059363a8bae74dfe5d567cbe30adc42823c4
Dhinakaranv01 <dhinavasudevan@gmail.com>
2023-12-01 18:51:20 +0530

    encapsulation program

---
+++ cricketer.java
@@ -0,0 +1,25 @@
+
+
+
+public class cricketer {
+
+    private String name;
+    private int age ;
+
+
+    public void setname(String newName){
+        name = newName;
+
+    }
+    public void setage(int newAge){
+        age = newAge;
+    }
+
+    public String getname(){
+        return name;
+    }
+    public int getage(){
+        return age;
+    }
+    
+}
---
+++ demo.java
@@ -0,0 +1,16 @@
+public class demo {
+    public static void main(String[]args){
+        cricketer c1 =new cricketer();
+        c1.setname("kholi");
+        c1.setage(50);
+
+
+        cricketer c2 = new cricketer();
+        c2.setname("dhoni");
+        c2.setage(40);
+
+
+        System.out.println(c1.getname());
+    }
+    
+}
